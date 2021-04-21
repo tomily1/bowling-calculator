bc5840105e45599027d93564366f409a876b2623
Israel Tomilayo <itomilayo@gmail.com>
2021-04-21 04:22:04 +0300

    move bowling logic into a separate controller

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,53 +3,5 @@
 class Game < ApplicationRecord
   belongs_to :user, inverse_of: :game
 
-  serialize :frames, Array
-
-  def roll(knocked_pins)
-    frames << knocked_pins
-  end
-
-  def calculate_score
-    score = 0
-    frame_index = 0
-
-    10.times do |_frame|
-      if strike?(frame_index)
-        score += (10 + strike_bonus(frame_index))
-        frame_index += 1
-      elsif spare?(frame_index)
-        score += (10 + spare_bonus(frame_index))
-        frame_index += 2
-      else
-        score += sum_of_balls_in_frame(frame_index)
-        frame_index += 2
-      end
-    end
-
-    score
-  end
-
-  def strike?(frame_index)
-    point_at(frame_index) == 10
-  end
-
-  def spare?(frame_index)
-    (point_at(frame_index) + point_at(frame_index + 1)) == 10
-  end
-
-  def sum_of_balls_in_frame(frame_index)
-    point_at(frame_index) + point_at(frame_index + 1)
-  end
-
-  def spare_bonus(frame_index)
-    point_at(frame_index + 2)
-  end
-
-  def strike_bonus(frame_index)
-    point_at(frame_index + 1) + point_at(frame_index + 2)
-  end
-
-  def point_at(index)
-    frames[index].to_i
-  end
+  include HasBowling
 end
